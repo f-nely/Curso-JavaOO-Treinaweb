f2332e3e62f13cf8163cedfed483c278f33d39bb
nely de faria <nelly.nf54@gmail.com>
2020-10-19 18:19:48 -0300

    Altera visibilidade dos atributos e cria gets e sets

--- tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
@@ -1,10 +1,38 @@
 package com.nelynely.javaoo.classes;
 
 public class Veiculo {
-	public String nome;
-	public String marca;
-	public String chassi;
-	public int quantidadeRodas;
+	private String nome;
+	private String marca;
+	private String chassi;
+	private int quantidadeRodas;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getChassi() {
+		return chassi;
+	}
+	
+	public int getQuantidadeRodas() {
+		return quantidadeRodas;
+	}
+	
+	public void setQuantidadeRodas(int quantidadeRodas) {
+		this.quantidadeRodas = quantidadeRodas;
+	}
 	
 	public void ligar() {
 		System.out.println("O veículo ligou: ");
