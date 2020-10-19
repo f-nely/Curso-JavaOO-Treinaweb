197a9b14f493fe8633c21f75c5bd955e3a9e314c
nely de faria <nelly.nf54@gmail.com>
2020-10-19 13:08:30 -0300

    Adiciona classe Main

---
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -0,0 +1,19 @@
+package com.nelynely.javaoo.main;
+
+import com.nelynely.javaoo.classes.Veiculo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Veiculo corsa = new Veiculo();
+		corsa.marca = "GM";
+		corsa.nome = "Corsa";
+		corsa.ligar();
+		
+		Veiculo celta = new Veiculo();
+		celta.marca = "GM";
+		celta.nome = "Celta";
+		celta.desligar();
+	}
+
+}
