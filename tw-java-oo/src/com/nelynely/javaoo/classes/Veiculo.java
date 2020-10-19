dad0f4d7b691298c0d51f387f1c0831e402dc78c
nely de faria <nelly.nf54@gmail.com>
2020-10-19 13:06:41 -0300

    Adiciona classe Veiculo

---
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
@@ -0,0 +1,16 @@
+package com.nelynely.javaoo.classes;
+
+public class Veiculo {
+	public String nome;
+	public String marca;
+	public String chassi;
+	public int quantidadeRodas;
+	
+	public void ligar() {
+		System.out.println("O veículo ligou: ");
+	}
+	
+	public void desligar() {
+		System.out.println("O veículo desligou: ");
+	}
+}
