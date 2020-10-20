96f0609731dd29a14aba3b3748f0f047addc8d0f
nely de faria <nelly.nf54@gmail.com>
2020-10-20 18:49:29 -0300

    Tratamento de erro

--- tw-java-oo/src/com/nelynely/javaoo/main/Main.java
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -5,19 +5,20 @@ import com.nelynely.javaoo.classes.Veiculo;
 public class Main {
 
 	public static void main(String[] args) {
-		Veiculo corsa = new Veiculo();
-		corsa.setMarca("GM");
-		corsa.setNome("Corsa");
-		corsa.abastecer(10);
-		System.out.println(corsa.getQuantidadeCombustivel());
-		System.out.println(corsa.getNome());
-		corsa.ligar();
+		try {
+			Veiculo corsa = new Veiculo();
+			corsa.setMarca("GM");
+			corsa.setNome("Corsa");
+			corsa.setChassi("ABCDE");
+			corsa.abastecer(10);
+			System.out.println(corsa.getQuantidadeCombustivel());
+			System.out.println(corsa.getNome());
+			corsa.ligar();
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro: " + e.getMessage());
+		}
 		
-		Veiculo celta = new Veiculo();
-		celta.setMarca("GM");
-		celta.setNome("Celta");
-		System.out.println(celta.getNome());
-		celta.desligar();
+		
 	}
 
 }
