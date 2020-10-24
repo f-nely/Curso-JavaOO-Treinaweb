5927ffa0324f7bbcf1b70f60b260a607aaf68437
nely de faria <nelly.nf54@gmail.com>
2020-10-24 18:38:44 -0300

    Comenta construtor padrão e usa construtor personalizado

--- tw-java-oo/src/com/nelynely/javaoo/main/Main.java
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -6,15 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Carro corsa = new Carro();
+			/*Carro corsa = new Carro();
 			corsa.setMarca("GM");
-			corsa.setNome("Corsa");
+			corsa.setNome("Corsa");*/
+			Carro corsa = new Carro("Corsa", "GM");
 			corsa.setChassi("ABCDE");
 			corsa.abastecer(10);
 			System.out.println(corsa.getNome());
 			System.out.println(corsa.getQuantidadeCombustivel());
 			System.out.println(corsa.getQuantidadeRodas());
 			corsa.ligar();
+			Carro celta = new Carro();
+			celta.desligar();
 		} catch (Exception e) {
 			System.out.println("Ocorreu um erro: " + e.getMessage());
 		}
