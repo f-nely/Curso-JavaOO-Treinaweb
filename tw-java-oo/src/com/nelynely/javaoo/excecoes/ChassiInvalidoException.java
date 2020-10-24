8c2c1824cb59de1032a061948914af2ce5c642dd
nely de faria <nelly.nf54@gmail.com>
2020-10-24 17:44:18 -0300

    Processo de serialização

--- tw-java-oo/src/com/nelynely/javaoo/excecoes/ChassiInvalidoException.java
+++ tw-java-oo/src/com/nelynely/javaoo/excecoes/ChassiInvalidoException.java
@@ -2,6 +2,8 @@ package com.nelynely.javaoo.excecoes;
 
 public class ChassiInvalidoException extends Exception {
 
+	private static final long serialVersionUID = 2056228056904801283L;
+
 	public ChassiInvalidoException(String chassi) {
 		super(String.format("Esse chassi é inválido [%s] ", chassi));
 	}
