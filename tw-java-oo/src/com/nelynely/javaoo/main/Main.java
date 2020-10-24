4d9cd00b56257f629ba97678c23687d37443f549
nely de faria <nelly.nf54@gmail.com>
2020-10-24 17:56:13 -0300

    Instacia classe Carro e remove importação veiculo

--- tw-java-oo/src/com/nelynely/javaoo/main/Main.java
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -1,15 +1,15 @@
 package com.nelynely.javaoo.main;
 
-import com.nelynely.javaoo.classes.Veiculo;
+import com.nelynely.javaoo.classes.Carro;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Veiculo corsa = new Veiculo();
+			Carro corsa = new Carro();
 			corsa.setMarca("GM");
 			corsa.setNome("Corsa");
-			corsa.setChassi("ABCE");
+			corsa.setChassi("ABCDE");
 			corsa.abastecer(10);
 			System.out.println(corsa.getQuantidadeCombustivel());
 			System.out.println(corsa.getNome());
