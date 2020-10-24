d45d2ece37eef91d581c8cad13782fe728e83012
nely de faria <nelly.nf54@gmail.com>
2020-10-24 18:37:03 -0300

    Sobrecarga de construtor

--- tw-java-oo/src/com/nelynely/javaoo/classes/Carro.java
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Carro.java
@@ -5,4 +5,10 @@ public class Carro extends Veiculo{
 		public Carro() {
 			this.quantidadeRodas = 4;
 		}
+		
+		public Carro(String nome, String marca) {
+			this.quantidadeRodas = 4;
+			this.setNome(nome);
+			this.setMarca(marca);
+		}
 }
