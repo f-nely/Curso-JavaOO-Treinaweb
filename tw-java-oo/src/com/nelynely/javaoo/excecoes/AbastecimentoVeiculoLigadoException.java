cd480659b540d6c03c3ddbe2ae8b48f3b0639403
nely de faria <nelly.nf54@gmail.com>
2020-10-26 19:35:09 -0300

    Adiciona classe AbastecimentoVeiculoException

---
+++ tw-java-oo/src/com/nelynely/javaoo/excecoes/AbastecimentoVeiculoLigadoException.java
@@ -0,0 +1,7 @@
+package com.nelynely.javaoo.excecoes;
+
+public class AbastecimentoVeiculoLigadoException extends Exception {
+
+	private static final long serialVersionUID = 3570370485923457556L;
+
+}
