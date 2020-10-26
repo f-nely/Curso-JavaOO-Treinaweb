25243a43c42f851056c9cfa446c1b6979ee25166
nely de faria <nelly.nf54@gmail.com>
2020-10-26 17:46:13 -0300

    Instancia classe Moto e usa método quantidadeRodas

--- tw-java-oo/src/com/nelynely/javaoo/main/Main.java
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -1,6 +1,7 @@
 package com.nelynely.javaoo.main;
 
 import com.nelynely.javaoo.classes.Carro;
+import com.nelynely.javaoo.classes.Moto;
 
 public class Main {
 
@@ -12,13 +13,15 @@ public class Main {
 			Carro corsa = new Carro("Corsa", "GM");
 			corsa.setChassi("ABCDE");
 			corsa.abastecer(10);
+			corsa.setQuantidadePortas(4);
 			System.out.println(corsa.getNome());
 			System.out.println(corsa.getQuantidadeCombustivel());
 			System.out.println(corsa.getQuantidadeRodas());
 			System.out.println(String.format("O veiculo %s está ligado? %b", corsa.getNome(), corsa.IsLigado()));
 			corsa.ligar();
-			Carro celta = new Carro();
-			celta.desligar();
+			Moto fazer = new Moto();
+			fazer.setMarca("Yamaha");
+			fazer.setNome("Fazer");
 		} catch (Exception e) {
 			System.out.println("Ocorreu um erro: " + e.getMessage());
 		}
