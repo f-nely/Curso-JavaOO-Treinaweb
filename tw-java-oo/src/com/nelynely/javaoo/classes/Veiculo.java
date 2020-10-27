4ba70439f1bef92af5c3b82dd6dfa3cb6f89522a
nely de faria <nelly.nf54@gmail.com>
2020-10-27 20:33:10 -0300

    Definindo os métodos abastecer, ligar e desligar como final

--- tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
@@ -68,19 +68,19 @@ public class Veiculo {
 		return velocidade;
 	}
 
-	public void ligar() {
+	public final void ligar() {
 		this.ligado = true;
 		this.velocidade = 0;
 		System.out.println("O veículo ligou: ");
 	}
 
-	public void desligar() {
+	public final void desligar() {
 		this.velocidade = 0;
 		this.ligado = false;
 		System.out.println("O veículo desligou: ");
 	}
 
-	public void abastecer(float litros) throws AbastecimentoVeiculoLigadoException {
+	public final void abastecer(float litros) throws AbastecimentoVeiculoLigadoException {
 		if (!this.ligado) {
 			quantidadeCombustivel += litros;
 		} else {
