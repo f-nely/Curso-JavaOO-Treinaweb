e57692dec23e3e674c80a061c1da471e3c78a916
nely de faria <nelly.nf54@gmail.com>
2020-10-27 18:50:10 -0300

    Adiciona classe AceleracaoVeiculoLigadoException

---
+++ tw-java-oo/src/com/nelynely/javaoo/excecoes/AceleracaoVeiculoLigadoException.java
@@ -0,0 +1,7 @@
+package com.nelynely.javaoo.excecoes;
+
+public class AceleracaoVeiculoLigadoException extends Exception {
+
+	private static final long serialVersionUID = 2227799542303692163L;
+
+}
