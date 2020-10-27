0f6793715a8987bd13431f220095b95bff3658a6
nely de faria <nelly.nf54@gmail.com>
2020-10-27 18:51:22 -0300

    Adiciona classe FrenagemVeiculoDesligadoException

---
+++ tw-java-oo/src/com/nelynely/javaoo/excecoes/FrenagemVeiculoDesligadoException.java
@@ -0,0 +1,7 @@
+package com.nelynely.javaoo.excecoes;
+
+public class FrenagemVeiculoDesligadoException extends Exception{
+
+	private static final long serialVersionUID = 8094708509204077897L;
+
+}
