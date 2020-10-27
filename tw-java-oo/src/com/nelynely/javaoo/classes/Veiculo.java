141d10212bf27e4eae8219d35c869f90471498eb
nely de faria <nelly.nf54@gmail.com>
2020-10-27 18:53:28 -0300

    Criação do atributo velocidade e dos métodos Acelerar e frear

--- tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Veiculo.java
@@ -1,7 +1,9 @@
 package com.nelynely.javaoo.classes;
 
 import com.nelynely.javaoo.excecoes.AbastecimentoVeiculoLigadoException;
+import com.nelynely.javaoo.excecoes.AceleracaoVeiculoLigadoException;
 import com.nelynely.javaoo.excecoes.ChassiInvalidoException;
+import com.nelynely.javaoo.excecoes.FrenagemVeiculoDesligadoException;
 
 public class Veiculo {
 	private String nome;
@@ -10,44 +12,46 @@ public class Veiculo {
 	protected int quantidadeRodas;
 	private float quantidadeCombustivel;
 	private Boolean ligado;
-	
+	protected float velocidade;
+
 	public Veiculo() {
 		this.ligado = false;
+		this.velocidade = 0;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getMarca() {
 		return marca;
 	}
-	
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-	
+
 	public String getChassi() {
 		return chassi;
 	}
-	
-	public void setChassi(String chassi) throws ChassiInvalidoException{
+
+	public void setChassi(String chassi) throws ChassiInvalidoException {
 		if (chassi.length() == 5) {
 			this.chassi = chassi;
 		} else {
 			throw new ChassiInvalidoException(chassi);
 		}
-		
+
 	}
-	
+
 	public int getQuantidadeRodas() {
 		return quantidadeRodas;
 	}
-	
+
 	public float getQuantidadeCombustivel() {
 		return quantidadeCombustivel;
 	}
@@ -60,22 +64,43 @@ public class Veiculo {
 		return ligado;
 	}
 
+	public float getVelocidade() {
+		return velocidade;
+	}
+
 	public void ligar() {
 		this.ligado = true;
+		this.velocidade = 0;
 		System.out.println("O veículo ligou: ");
 	}
-	
+
 	public void desligar() {
+		this.velocidade = 0;
 		this.ligado = false;
 		System.out.println("O veículo desligou: ");
 	}
-	
-	public void abastecer(float litros) throws AbastecimentoVeiculoLigadoException{
-		if(!this.ligado) {
+
+	public void abastecer(float litros) throws AbastecimentoVeiculoLigadoException {
+		if (!this.ligado) {
 			quantidadeCombustivel += litros;
 		} else {
 			throw new AbastecimentoVeiculoLigadoException();
 		}
-		
+	}
+
+	public void acelerar() throws AceleracaoVeiculoLigadoException {
+		if (this.ligado) {
+			this.velocidade += 10;
+		} else {
+			throw new AceleracaoVeiculoLigadoException();
+		}
+	}
+
+	public void frear() throws FrenagemVeiculoDesligadoException {
+		if (this.ligado) {
+			this.velocidade -= 10;
+		} else {
+			throw new FrenagemVeiculoDesligadoException();
+		}
 	}
 }
