3d0dbc794426e1a1c6e2c8593d2a35fd904f638e
nely de faria <nelly.nf54@gmail.com>
2020-10-27 18:56:22 -0300

    Utilização dos métodos acelerar e frear

--- tw-java-oo/src/com/nelynely/javaoo/main/Main.java
+++ tw-java-oo/src/com/nelynely/javaoo/main/Main.java
@@ -1,6 +1,7 @@
 package com.nelynely.javaoo.main;
 
 import com.nelynely.javaoo.classes.Carro;
+import com.nelynely.javaoo.classes.Moto;
 import com.nelynely.javaoo.excecoes.AbastecimentoVeiculoLigadoException;
 import com.nelynely.javaoo.excecoes.ChassiInvalidoException;
 
@@ -18,10 +19,18 @@ public class Main {
 			System.out.println(String.format("O veículo %s está ligado? %b", corsa.getNome(), corsa.IsLigado()));
 			corsa.abastecer(10);
 			corsa.ligar();
-			//corsa.abastecer(10);
-			//Moto fazer = new Moto();
-			//fazer.setMarca("Yamaha");
-			//fazer.setNome("Fazer");
+			corsa.acelerar();
+			System.out.println(String.format("A velocidade do veículo: %.2f ", corsa.getVelocidade()));
+			corsa.frear();
+			System.out.println(String.format("A velocidade do veículo: %.2f ", corsa.getVelocidade()));
+			Moto fazer = new Moto();
+			fazer.setMarca("Yamaha");
+			fazer.setNome("Fazer");
+			fazer.ligar();
+			fazer.acelerar();
+			System.out.println(String.format("A velocidade do veículo: %.2f ", fazer.getVelocidade()));
+			fazer.frear();
+			System.out.println(String.format("A velocidade do veículo: %.2f ", fazer.getVelocidade()));
 		} catch (AbastecimentoVeiculoLigadoException e) {
 			System.out.println("Você não pode abastecer um veículo enquanto ele estiver ligado");
 		} catch (ChassiInvalidoException e) {
