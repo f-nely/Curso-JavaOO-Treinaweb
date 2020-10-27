caf1117b0d82d01a148b5bc18fd6f4fe68c0f35c
nely de faria <nelly.nf54@gmail.com>
2020-10-27 18:55:22 -0300

    Criação de mais um construtor e sobrescrita dos métodos acelerar e frear

--- tw-java-oo/src/com/nelynely/javaoo/classes/Moto.java
+++ tw-java-oo/src/com/nelynely/javaoo/classes/Moto.java
@@ -1,9 +1,36 @@
 package com.nelynely.javaoo.classes;
 
+import com.nelynely.javaoo.excecoes.AceleracaoVeiculoLigadoException;
+import com.nelynely.javaoo.excecoes.FrenagemVeiculoDesligadoException;
+
 public class Moto extends Veiculo{
 	
 	public Moto() {
 		this.quantidadeRodas = 2;
+	}
+	
+	public Moto(String nome, String marca) {
+		this.quantidadeRodas = 2;
+		this.setNome(nome);
+		this.setMarca(marca);
+	}
+	
+	@Override
+	public void acelerar() throws AceleracaoVeiculoLigadoException {
+		if (this.IsLigado()) {
+			this.velocidade += 3;
+		} else {
+			throw new AceleracaoVeiculoLigadoException();
+		}
+	}
+	
+	@Override
+	public void frear() throws FrenagemVeiculoDesligadoException {
+		if (this.IsLigado()) {
+			this.velocidade -= 3;
+		} else {
+			throw new FrenagemVeiculoDesligadoException();
+		}
 	}
 
 }
